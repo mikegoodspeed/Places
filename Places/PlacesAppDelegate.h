d2c0f9afbfb5dd829f969df237f2de648c859c65
Mike Goodspeed <mikegoodspeed@gmail.com>
2011-07-09 22:55:48 +0100

    initial checkin

---
+++ Places/PlacesAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PlacesAppDelegate.h
+//  Places
+//
+//  Created by Mike Goodspeed on 7/9/11.
+//  Copyright 2011 Mike Goodspeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlacesAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
